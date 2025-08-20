2b97b134e82a267b23345a1297ca9a40b195c86c
Maisa <ms.maisa.santos@gmail.com>
2025-08-20 00:47:14 -0300

    novo

---
+++ index.js
@@ -0,0 +1,11 @@
+const popup = document.querySelector(".popup");
+const edit = document.querySelector(".profile__edit-button");
+
+edit addEventListener("click", openpopup);
+
+function openpopup (){
+  console.log("clicou")
+// popup.classList.remove('popup');
+// popup.classList.add('popup--active');
+}
+
