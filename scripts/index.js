245bae012fe7a44170d9f33cc318bdb2029f12f3
Maisa Santos de Lana <msantos@eslsistemas.com.br>
2025-09-01 22:17:26 -0300

    Adiciona o modal da galeria

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,58 @@
+//popup
+const popupProfile = document.querySelector("#popup__profile");
+const popupGallery = document.querySelector("#popup__gallery");
+
+// button
 const editButton = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
-const closePopup = document.querySelector(".popup__button-close");
+const addButton = document.querySelector(".profile__add-button");
+const closePopupProfile = popupProfile.querySelector(".popup__button-close");
+const closePopupGallery = popupGallery.querySelector(".popup__button-close");
+
+//popup-elements
 const popupName = document.querySelector("#name");
 const popupDescription = document.querySelector("#description");
 const userName = document.querySelector(".profile__name");
 const userDescription = document.querySelector(".profile__profession");
-const formElement = document.querySelector(".popup__form");
-const likes = document.querySelectorAll(".material-symbols-outlined")
+const formElement = popupProfile.querySelector(".popup__form");
 
-function openPopup (){
-popupName.value = userName.textContent;
-popupDescription.value = userDescription.textContent;
-popup.classList.remove('popup');
-popup.classList.add('popup--active');
+//gallery
+const likes = document.querySelectorAll(".material-symbols-outlined");
+
+
+// generical function
+function openPopup (openPopup) {
+openPopup.classList.remove('popup');
+openPopup.classList.add('popup--active');
 }
 
-function closedPopup (){
-popup.classList.add('popup');
-popup.classList.remove('popup--active');
+function closedPopup (closedPopup){
+closedPopup.classList.add('popup');
+closedPopup.classList.remove('popup--active');
 }
 
 function handleProfileFormSubmit(evt){
 evt.preventDefault();
 userName.textContent = popupName.value;
 userDescription.textContent = popupDescription.value;
-closedPopup();
+closedPopup(popupProfile);
 }
 
-function like(){
-    likes.classList.add("material-symbols-rounded");
-}
-
-editButton.addEventListener("click", openPopup);
-closePopup.addEventListener("click", closedPopup);
+// Profile function
+editButton.addEventListener("click", () => {
+popupName.value = userName.textContent;
+popupDescription.value = userDescription.textContent;
+openPopup(popupProfile);
+});
+closePopupProfile.addEventListener("click", () => { closedPopup(popupProfile)});
 formElement.addEventListener("submit", handleProfileFormSubmit);
-likes.addEventListener("click", liked)
 
+//Gallery function
+addButton.addEventListener("click", () => {openPopup(popupGallery)});
+closePopupGallery.addEventListener("click", () => { closedPopup(popupGallery)});
 
+//function like(){
+//    likes.classList.add("material-symbols-rounded");
+//}
+// likes.addEventListener("click", liked)
 
+//fazer commit
