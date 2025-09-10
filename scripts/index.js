9f47b5a16208be578d621dbedc9e0386fae9dbeb
Maisa Santos de Lana <msantos@eslsistemas.com.br>
2025-09-09 21:36:23 -0300

    correção do merge

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,42 @@
-//popup
+//Popups
 const popupProfile = document.querySelector("#popup__profile");
 const popupGallery = document.querySelector("#popup__gallery");
 
-// button
+//buttons
 const editButton = document.querySelector(".profile__edit-button");
 const addButton = document.querySelector(".profile__add-button");
 const closePopupProfile = popupProfile.querySelector(".popup__button-close");
 const closePopupGallery = popupGallery.querySelector(".popup__button-close");
 
-//popup-elements
+
+//Popup__Elements
 const popupName = document.querySelector("#name");
 const popupDescription = document.querySelector("#description");
 const userName = document.querySelector(".profile__name");
 const userDescription = document.querySelector(".profile__profession");
-const formElement = popupProfile.querySelector(".popup__form");
+const formElement = document.querySelector(".popup__form");
+const imageLink = popupGallery.querySelector("#link_image");
+const imageTitle = popupGallery.querySelector("#title_image");
+const galleryForm = popupGallery.querySelector(".popup__form");
+const galleryButton = popupGallery.querySelector(".popup__button");
 
-//gallery
+//Gallery__Elements
 const likes = document.querySelectorAll(".material-symbols-outlined");
+const card = document.querySelector(".gallery");
+const popupImage = document.querySelector(".popup__image");
+const closePopupImage = popupImage.querySelector(".image__close");
+const galleryImages = card.querySelectorAll(".gallery__image");
 
 
-// generical function
-function openPopup (openPopup) {
+//Function general
+function openedPopup (openPopup){
 openPopup.classList.remove('popup');
 openPopup.classList.add('popup--active');
 }
 
-function closedPopup (closedPopup){
-closedPopup.classList.add('popup');
-closedPopup.classList.remove('popup--active');
+function closedPopup (closePopup){
+closePopup.classList.add('popup');
+closePopup.classList.remove('popup--active');
 }
 
 function handleProfileFormSubmit(evt){
@@ -37,22 +46,87 @@ userDescription.textContent = popupDescription.value;
 closedPopup(popupProfile);
 }
 
-// Profile function
+
+//profile
 editButton.addEventListener("click", () => {
 popupName.value = userName.textContent;
 popupDescription.value = userDescription.textContent;
-openPopup(popupProfile);
+ openedPopup(popupProfile)});
+
+closePopupProfile.addEventListener("click", () => { closedPopup (popupProfile)
 });
-closePopupProfile.addEventListener("click", () => { closedPopup(popupProfile)});
 formElement.addEventListener("submit", handleProfileFormSubmit);
 
-//Gallery function
-addButton.addEventListener("click", () => {openPopup(popupGallery)});
-closePopupGallery.addEventListener("click", () => { closedPopup(popupGallery)});
+//Gallery
+addButton.addEventListener("click", () =>{ openedPopup (popupGallery)
+});
+closePopupGallery.addEventListener("click", () => { closedPopup (popupGallery)
+});
 
-//function like(){
-//    likes.classList.add("material-symbols-rounded");
-//}
-// likes.addEventListener("click", liked)
 
-//fazer commit
+//Gallery_likes
+likes.forEach(like => {
+like.addEventListener("click", () => {
+like.classList.toggle("material-symbols-rounded");  
+like.classList.toggle("material-symbols-outlined"); 
+});
+});
+
+function addCard (title, link){
+    const template = 
+    document.querySelector("#gallery__template")
+    .content.querySelector(".gallery__item");
+
+    const cardElement = template.cloneNode(true);
+    const cardTitle = cardElement.querySelector(".gallery__image-name");
+    const cardLink = cardElement.querySelector(".gallery__image");
+
+    cardTitle.textContent = title;
+    cardLink.src = link;
+
+    const trashButton = cardElement.querySelector(".delete.material-symbols-rounded");
+    trashButton.addEventListener("click", () => {     
+    trashButton.closest(".gallery__item").classList.toggle("hidden");
+   });
+
+    const likeButton = cardElement.querySelector(".material-symbols-outlined");
+    likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("material-symbols-rounded");
+    likeButton.classList.toggle("material-symbols-outlined");
+  });
+
+    card.prepend(cardElement);
+}
+
+function handlegalleryformsubmit(evt){
+evt.preventDefault ();
+addCard (imageTitle.value, imageLink.value);
+closedPopup (popupGallery);
+}
+
+galleryForm.addEventListener("submit", handlegalleryformsubmit);
+
+//Delete image
+const trashs = document.querySelectorAll(".delete.material-symbols-rounded");
+trashs.forEach(trash => {
+    trash.addEventListener("click", () => {     
+    trash.closest(".gallery__item").classList.toggle("hidden");
+});
+})
+
+//Popup image
+
+const popupImg = document.querySelector(".popup__image-expand");
+galleryImages.forEach(galleryImage => {
+  galleryImage.addEventListener("click", () => {
+    const imageName = galleryImage.closest(".gallery__item").querySelector(".gallery__image-name").textContent;
+    const imageFooter = document.querySelector(".popup__image-footer");
+    popupImg.src = galleryImage.src;
+    popupImg.alt = galleryImage.alt;
+    imageFooter.textContent = imageName;
+
+    popupImage.setAttribute("style","display: flex");
+  });
+});
+
+closePopupImage.addEventListener("click", () => {popupImage.setAttribute("style","display: none")});
